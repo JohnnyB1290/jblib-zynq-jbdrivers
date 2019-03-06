34bd1a88d0a5b644cb3b2c5be91696fd7b7b9575
Stalker1290 <Stalker1290@mail.ru>
2019-03-06 19:40:20 +0300

    Add critical section define

--- Includes/chip.h
+++ Includes/chip.h
@@ -44,6 +44,10 @@ typedef bool (*Out_packet_callback_t)(EthernetFrame* Frame_ptr, uint16_t frame_s
 #define __disable_irq() Xil_ExceptionDisable()
 #define __enable_irq()	Xil_ExceptionEnable()
 
+#define CRITICAL_SECTION(code) { __disable_irq(); \
+								code \
+								__enable_irq(); }
+
 #if !defined(MAX)
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #endif
